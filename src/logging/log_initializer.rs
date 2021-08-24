66c52cdd242d9bab6104077f146ba5c3249de41a
Aditya Mahaddalkar <adityam1311@gmail.com>
2021-08-24 07:05:51 +0530

    Added test for log initializer

--- src/logging/log_initializer.rs
+++ src/logging/log_initializer.rs
@@ -4,17 +4,32 @@ use env_logger;
 use log::{info};
 use std::env::VarError;
 
+const RUST_LOG_KEY: &'static str = "RUST_LOG";
+
 pub fn log_init() {
-    match env::var("RUST_LOG") {
+    match env::var(RUST_LOG_KEY) {
         Ok(_) => {
             info!("action=log_init, message=RUST_LOG env variable found");
         }
         Err(_) => {
             info!("action=log_init, message=RUST_LOG env variable not found, setting to default INFO");
-            env::set_var("RUST_LOG", "INFO");
+            env::set_var(RUST_LOG_KEY, "INFO");
         }
     }
 
     env_logger::init();
     info!("action=log_init, message=logger initialized");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_log_init_env_set() {
+        super::log_init();
+        let expected = String::from("INFO");
+        let actual = env::var(RUST_LOG_KEY).unwrap();
+        assert_eq!(expected, actual);
+    }
 }
